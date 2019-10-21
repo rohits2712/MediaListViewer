62fa989560671ed0c67e95722e4be637a43c71a6
Rohit Sharma <rohitrises@gmail.com>
2019-10-22 03:34:50 +0530

    FormsModule Implementation

--- MediaListViewer/src/app/app.module.ts
+++ MediaListViewer/src/app/app.module.ts
@@ -3,12 +3,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item/media-item.component';
 import{CategoryListPipe} from './category-list.pipe';
+import {FormsModule} from '@angular/forms';
+import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
 
 @NgModule({
   imports:[
-    BrowserModule
+    BrowserModule,FormsModule
   ], //other modules that it will need
-  declarations:[AppComponent, MediaItemComponent,CategoryListPipe], //components pipes directives that dont come from another module 
+  declarations:[AppComponent, MediaItemComponent,CategoryListPipe, MediaItemFormComponent], //components pipes directives that dont come from another module 
   bootstrap:[AppComponent] //bootstrap used for route module, entry point for app code
 })
 export class AppModule {}
