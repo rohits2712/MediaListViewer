8d6492907ba40a47066e9a65b1960e75f6daa75c
Rohit Sharma <rohitrises@gmail.com>
2019-10-22 04:32:37 +0530

    Validation and error handling

--- MediaListViewer/src/app/app.module.ts
+++ MediaListViewer/src/app/app.module.ts
@@ -3,12 +3,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item/media-item.component';
 import{CategoryListPipe} from './category-list.pipe';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
 
 @NgModule({
   imports:[
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,ReactiveFormsModule
   ], //other modules that it will need
   declarations:[AppComponent, MediaItemComponent,CategoryListPipe, MediaItemFormComponent], //components pipes directives that dont come from another module 
   bootstrap:[AppComponent] //bootstrap used for route module, entry point for app code
--- MediaListViewer/src/app/media-item-form/media-item-form.component.ts
+++ MediaListViewer/src/app/media-item-form/media-item-form.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import{FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-media-item-form',
   templateUrl: './media-item-form.component.html',
   styleUrls: ['./media-item-form.component.css']
 })
-export class MediaItemFormComponent {
+export class MediaItemFormComponent implements OnInit{
+  form:FormGroup;
+
+  ngOnInit(){
+    this.form = new FormGroup({
+       medium: new FormControl('Movies'),
+      //  name: new FormControl('',Validators.pattern('[\\w\\-\\s\\/]+')),
+      name: new FormControl('',Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+       category: new FormControl(),
+       year: new FormControl()
+    });
+  }
+
+  
+
   onSubmit(mediaItem){
     console.log(mediaItem);
   }
